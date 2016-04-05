49511d826e15385f33fe70b99bdcd0e2c8214657
CKiilu <cndeti@gmail.com>
2016-04-05 16:42:41 +0300

    Parsed geospatial data to get lng, lat

--- app/views.py
+++ app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+import googlemaps
+from datetime import datetime
+
+gmaps = googlemaps.Client(key='AIzaSyD0MSmH6-nAgxIeJhO_SBiNjrbKyPHhQ9k')
+def home(request):
+	# Look up an address with reverse geocoding
+	reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+	# Geocoding an address
+	geocode_result1 = gmaps.geocode('Korogocho, Kenya')[0]['geometry']['location']['lat']
+	geocode_result2 = gmaps.geocode('Korogocho, Kenya')[0]['geometry']['location']['lng']
+
+	# Request directions via public transit
+	now = datetime.now()
+	directions_result = gmaps.directions("Sydney Town Hall",
+                                     "Parramatta, NSW",
+                                     mode="transit",
+                                     departure_time=now)
+	context = {
+		"reverse":geocode_result1,
+		"reverse2":geocode_result2,
+	}
+	return render(request, "home.html", context)
